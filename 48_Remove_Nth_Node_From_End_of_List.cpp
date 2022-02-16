e0122096d6ecc5456b64fdc6ca339f636d12ab8f
ARPIT KHANULIA <arpitkhanulia7@gmail.com>
2022-02-16 23:25:51 +0530

    java code

--- 48_Remove_Nth_Node_From_End_of_List.cpp
+++ 48_Remove_Nth_Node_From_End_of_List.cpp
@@ -1,58 +1,93 @@
-#include <iostream>
-#include <vector>
-#include <chrono>
-using namespace std;
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/submissions/
 
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
 
-class Solution
-{
-public:
-    ListNode *removeNthFromEnd(ListNode *head, int n)
-    {
+ 
 
-        if (head->next == NULL)
-            return NULL;
+// Example 1:
 
-                ListNode *temp = head;
-        int count = 0;
 
-        while (temp != NULL)
-        {
-            temp = temp->next;
-            count++;
-        }
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+// Example 2:
 
-        int no = count - n;
-        temp = head;
+// Input: head = [1], n = 1
+// Output: []
+// Example 3:
 
-        if (no == 0)
-            return head->next;
+// Input: head = [1,2], n = 1
+// Output: [1]
 
-        while (--no)
-            temp = temp->next;
+// struct ListNode
+// {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
 
-        temp->next = temp->next->next;
+// c++ code by method 1
 
-        return head;
-    }
-};
-int main()
-{
-    auto begin = chrono::high_resolution_clock::now();
-    //*********************************************************************************
+// class Solution
+// {
+// public:
+//     ListNode *removeNthFromEnd(ListNode *head, int n)
+//     {
 
-    //*********************************************************************************
-    auto end = chrono::high_resolution_clock::now();
-    auto elapsed = chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
-    cout << endl
-         << "Time measured: " << elapsed.count() * 1e-9;
-    return 0;
-}
+//         if (head->next == NULL)
+//             return NULL;
+
+//                 ListNode *temp = head;
+//         int count = 0;
+
+//         while (temp != NULL)
+//         {
+//             temp = temp->next;
+//             count++;
+//         }
+
+//         int no = count - n;
+//         temp = head;
+
+//         if (no == 0)
+//             return head->next;
+
+//         while (--no)
+//             temp = temp->next;
+
+//         temp->next = temp->next->next;
+
+//         return head;
+//     }
+// };
+
+// java code by method 2
+
+// class Solution {
+//     public ListNode removeNthFromEnd(ListNode head, int n) {
+
+//         if(head.next == null)
+//             return null;
+
+//         ListNode blank = new ListNode();
+//         blank.next = head;
+//         ListNode  tortoise = blank;
+//         ListNode hare = head;
+
+//         while(n>1){
+//             n--;
+//             hare = hare.next;
+//         }
+
+//         while(hare.next!= null) {
+//             hare = hare.next;
+//             tortoise= tortoise.next;
+//         }
+
+//         tortoise.next = (tortoise.next).next;
+
+//         return blank.next;
+
+//     }
+// }
